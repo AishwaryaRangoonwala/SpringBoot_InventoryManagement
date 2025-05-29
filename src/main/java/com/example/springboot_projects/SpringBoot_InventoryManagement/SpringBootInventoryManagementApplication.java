37472ba98707aa80f89acdba665222348c2cc68b
Aishwarya Rangoonwala <aishwaryarangoonwala@Aishwaryas-Laptop.local>
2025-05-29 09:52:14 +0530

    Inventory Management Starter Code

---
+++ src/main/java/com/example/springboot_projects/SpringBoot_InventoryManagement/SpringBootInventoryManagementApplication.java
@@ -0,0 +1,14 @@
+package com.example.springboot_projects.SpringBoot_InventoryManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInventoryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootInventoryManagementApplication.class, args);
+		System.out.println("*************** INVENTORY MANAGEMENT ***************");
+	}
+
+}
---
+++ src/test/java/com/example/springboot_projects/SpringBoot_InventoryManagement/SpringBootInventoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_projects.SpringBoot_InventoryManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootInventoryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
